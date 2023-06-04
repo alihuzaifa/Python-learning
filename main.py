471e7c09358ab91255194e092cd39f29982ebb2d
alihuzaifa <ha4587323@gmail.com>
2023-06-04 21:41:28 +0500

    Studing logical operator if else conditions and variables

---
+++ main.py
@@ -0,0 +1,84 @@
+# Print is like console in javascript
+# Variables in Python
+
+
+# firstName = "Ali"
+# lastName = "Huzaifa"
+# print('Hello',firstName,lastName);
+# isAdult = True
+# print('Huzaifa is Adult',isAdult)
+# isAdult = False
+# print('Huzaifa is Adult',isAdult)
+# firstName = 'Tony'
+# lastName = "Stark"
+# age = 51
+# isGenius = True
+# print(firstName,lastName,age,isGenius)
+
+# input is like prompt in javascript
+# greetUser = input('What is your name buddy?')
+# print("Hello",greetUser)
+
+# int convert string in number
+# float convert number into floating point number
+# str convert number into string
+
+# old_age = input('What is your age ?')
+# new_age = int(old_age) + 22
+# print(float(new_age))
+
+# String Methods
+# name = "Ali Huzaifa"
+# print(name.find("A"))
+# print(name.replace('Huzaifa',"Iron Name"))
+# print(name)
+# print(name.index(('f')))
+# print("z" in name)
+
+
+# Arithemetic Opeartors it is same like javascript
+# sum = 18 % 2
+# print(sum)
+# sum = 10
+# sub = 20
+# total = 200
+# total += sub + sum
+# print(total)
+
+
+# Logical Operator
+# or is like || and is like && not is like !=
+# print(2>2 or 2>1)
+# print(2>2 and 2>1)
+
+# If Condition
+# age = 6
+# if age > 100:
+#     print('You are an adult')
+#     print('Okay')
+# elif age < 18 and age > 3:
+#     print("You are in school")
+# else:print('you are a kid')
+#
+
+
+# Calculator In Python Programming First Task
+# firstInput = input('Enter your First Number')
+# operator = input('Enter your operator')
+# lastInput = input('Enter your last digit')
+# if operator == "+":
+#    sum = int(firstInput) + int(lastInput)
+#    print('Your Total Sum is',sum)
+# elif operator == "-":
+#     sub = int(firstInput) - int(lastInput)
+#     print('Your Total Deduction is', sub)
+# elif operator == "*":
+#     multiply = int(firstInput) * int(lastInput)
+#     print('Your Total production Amount is', multiply)
+# elif operator == "/":
+#     divide = int(firstInput) / int(lastInput)
+#     print('Your Total Divided Amount is', divide)
+# else:print("Your Total Percentage is",int(firstInput) % int(lastInput))
+
+
+# Calculator has been done and complete by me Alhumdulillah
